897815062933d7a9606a74686ca0a433e079ebc8
Carlos Meza <carlos@digitalr00ts.com>
2017-10-22 12:44:17 -0700

    better debug

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+set -e
+
 [ "${TRAVIS_PULL_REQUEST}" != 'false' ] && exit
 [ "${TRAVIS_BRANCH}" != 'develop' ] && exit
 
@@ -12,7 +15,12 @@ openssl aes-256-cbc -K $encrypted_d816d8b9a19e_key -iv $encrypted_d816d8b9a19e_i
 chmod 700 ~/.ssh/
 chmod 400 ~/.ssh/id_rsa
 
+echo "Fetching and checking out ${BIT_BASE} branch"
 git fetch origin ${GIT_BASE}:${GIT_BASE}
 git checkout ${GIT_BASE}
+echo
+echo "Merging $TRAVIS_COMMIT"
 git merge --ff-only "$TRAVIS_COMMIT"
+echo
+echo "Pushing changes"
 git push ${GIT_REPO_PUSH}
